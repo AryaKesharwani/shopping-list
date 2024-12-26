a6afa1b648dfbadc0bd7f45ae055d7d6aef892f5
Arya kesarwani <aryakesharwani1234@gmail.com>
2024-12-26 12:31:54 +0530

    initial

---
+++ app/(auth)/_layout.tsx
@@ -0,0 +1,8 @@
+import { Stack } from 'expo-router';
+export default function AuthRoutesLayout(){
+    return (
+        <Stack>
+            <Stack.Screen name="index"/>
+        </Stack>
+    )
+}
---
+++ app/(index)/index.tsx
@@ -0,0 +1,9 @@
+import { ThemedText } from "@/components/ThemedText"
+import { View } from "react-native"
+export default function HomeScreen(){
+    return (
+        <View>
+            <ThemedText type="title">Home In</ThemedText>
+        </View>
+    )
+}
